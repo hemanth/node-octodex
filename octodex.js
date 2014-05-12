45c2a474141738e081b15b892ea7927cf5b719a1
Eduardo Bohrer <nbluisrs@gmail.com>
2014-05-12 18:59:02 -0300

    adding cache feature

--- octodex.js
+++ octodex.js
@@ -1,17 +1,44 @@
-var request = require('request');
-var cheerio = require('cheerio');
-var url = 'http://octodex.github.com';
+var request = require('request'),
+    cheerio = require('cheerio'),
+    cache = require("memory-cache"),
+    url = 'http://octodex.github.com'
+    cacheKey = 'cache-key',
+    cacheTimeout = 1000 * 60 * 60; // 1 hour
 
-var img = function img(cb){ 
+var parsePage = function(cb) {
   request(url, function (error, response, html) {
     if (!error && response.statusCode == 200) {
       var $ = cheerio.load(html);
       var imgs = $('a.preview-image > img');
-      cb(null,url + imgs[Math.floor(Math.random() * (imgs.length))].data.src);
+      cb(null, imgs);
     } else {
-      cb(error,response.statusCode);
+      cb(error);
     }
   });
-}
+};
+
+var loadImages = function(cb, useCache) {
+  var imgs = useCache ? cache.get(cacheKey) : null;
+
+  if (imgs) {
+    cb(null, imgs);
+  } else {
+    parsePage(function(error, imgs) {
+      if (!error && useCache) 
+        cache.put(cacheKey, imgs, cacheTimeout);
+
+      cb(error, imgs);
+    });
+  }
+};
+
+var img = function img(cb, useCache){ 
+  loadImages(function(error, imgs) {
+    if (!error) 
+      cb(null, url + imgs[Math.floor(Math.random() * (imgs.length))].data.src);
+    else
+      cb(error);
+  }, useCache);
+};
 
 module.exports = {img: img};
